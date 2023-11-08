b3894126752860c7f2b9740a794eaa89fd56c670
urbain32 <urbainq32@gmail.com>
2023-11-08 11:14:43 +0200

    creatin a function that count NumericForString

--- app.js
+++ app.js
@@ -10,5 +10,13 @@
         $scope.totalNameValue = 0; // get the total value
         $scope.totalValue = totalNameValue;
       };
+      function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0; i < string.length; i++) {
+          totalStringValue += string.charCodeAt(i);
+        }
+
+        return totalStringValue;
+      }
     });
 })();
