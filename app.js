1d0a696a2ad715e7b3a01fba73837d7192ea7f4a
urbain32 <urbainq32@gmail.com>
2023-11-09 10:02:26 +0200

    adding the js to learn expression and intepretation

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 (function () {
   "use strict";
-  angular.module("DIApp", []).controller("DIController", DIController);
-  function DIController($scope, $filter) {
-    $scope.name = "Urban";
-    $scope.upper = function () {
-      var upCase = $filter("uppercase");
-      $scope.name = upCase($scope.name);
-    };
+
+  angular.module("MsgApp", []).controller("MsgController", MsgController);
+  function MsgController($scope) {
+    $scope.name = "Yaakov";
   }
 })();
