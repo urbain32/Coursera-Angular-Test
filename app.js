98ba422863235d65f9b20c2bf5cadba2b25383c8
urbain32 <urbainq32@gmail.com>
2023-11-09 09:49:32 +0200

    using minimize in javascript

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-(function () {
+// (function () {
+//   "use strict";
+//   angular.module("DIApp", []).controller("DIController", DIController);
+//   DIController.$inject = ["$scope", "$filter"];
+//   function DIController($scope, $filter) {
+//     $scope.name = "Urban";
+//     $scope.upper = function () {
+//       var upCase = $filter("uppercase");
+//       $scope.name = upCase($scope.name);
+//     };
+//   }
+// })();
+
+// this is a mifiefied javascript from the commented code
+//   DIController.$inject = ["$scope", "$filter"]; we use inject here coz miify can not recognize "$scope", "$filter"
+
+!(function () {
   "use strict";
-  angular.module("DIApp", []).controller("DIController", DIController);
-  function DIController($scope, $filter) {
-    $scope.name = "Urban";
-    $scope.upper = function () {
-      var upCase = $filter("uppercase");
-      $scope.name = upCase($scope.name);
-    };
+  function e(e, n) {
+    (e.name = "Urban"),
+      (e.upper = function () {
+        var r = n("uppercase");
+        e.name = r(e.name);
+      });
   }
+  angular.module("DIApp", []).controller("DIController", e),
+    (e.$inject = ["$scope", "$filter"]);
 })();
