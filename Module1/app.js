9a6eb0ad927ef2727f8c68c41450154dd89fd3cf
urbain32 <urbainq32@gmail.com>
2023-11-21 17:19:37 +0200

    remaing with one folder

--- Module1/app.js
+++ Module1/app.js
@@ -1,23 +1,49 @@
 (function () {
   "use strict";
-
   angular
-    .module("LunchCheck", [])
-    .controller("LunchCheckController", function ($scope) {
-      $scope.checkLunch = function () {
-        if (!$scope.items) {
-          $scope.message = "Please enter data first!";
-          return;
-        }
+    .module("ShoppingListCheckOff", [])
+    .controller("ToBuyController", ToBuyController)
+    .controller("AlreadyBoughtController", AlreadyBoughtController)
+    .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
 
-        var items = $scope.items.split(",");
-        var numItems = items.length;
+  ToBuyController.$inject = ["ShoppingListCheckOffService"];
+  AlreadyBoughtController.$inject = ["ShoppingListCheckOffService"];
+  // creating the function for ToBuyController
+  function ToBuyController(ShoppingListCheckOffService) {
+    var toBuyList = this; // creating the function for AlreadyBoughtController
+    toBuyList.items = ShoppingListCheckOffService.getToBuyitems();
+    toBuyList.buyItem = function (itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
+  }
+  // creating the function for AlreadyBoughtController
+  function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var alreadyBoughtList = this;
+    alreadyBoughtList.items =
+      ShoppingListCheckOffService.getAlreadyBoughtItems();
+  }
 
-        if (numItems <= 3) {
-          $scope.message = "Enjoy!";
-        } else {
-          $scope.message = "Too much!";
-        }
-      };
-    });
+  // creating our function service which is a singleton
+  function ShoppingListCheckOffService() {
+    var service = this;
+    var toBuyItems = [
+      { name: "papayi", quantity: 10 },
+      { name: "cokes", quantity: 2 },
+      { name: "Inzoga", quantity: 6 },
+      { name: "apples", quantity: 4 },
+      { name: "bananas", quantity: 7 },
+    ];
+    var alreadyBoughtItems = [];
+    service.buyItem = function (itemIndex) {
+      var item = toBuyItems[itemIndex];
+      alreadyBoughtItems.push(item);
+      toBuyItems.splice(itemIndex, 1);
+    };
+    service.getToBuyitems = function () {
+      return toBuyItems;
+    };
+    service.getAlreadyBoughtItems = function () {
+      return alreadyBoughtItems;
+    };
+  }
 })();
