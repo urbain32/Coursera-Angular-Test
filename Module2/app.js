90c4b134a9f7f4da035fddc31760497bea4dd565
urbain32 <urbainq32@gmail.com>
2023-11-21 16:20:41 +0200

    creating variable array

--- Module2/app.js
+++ Module2/app.js
@@ -6,5 +6,18 @@
       .controller("ToBuyController", ToBuyController)
       .controller("AlreadyBoughtController", AlreadyBoughtController)
       .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+    // creating our function service which is a singleton
+    function ShoppingListCheckOffService() {
+      var service = this;
+      var toBuyItems = [
+        { name: "papayi", quantity: 10 },
+        { name: "cokes", quantity: 2 },
+        { name: "Inzoga", quantity: 6 },
+        { name: "apples", quantity: 4 },
+        { name: "bananas", quantity: 7 },
+      ];
+      var alreadyBoughtItems = [];
+    }
   })();
 }
