975e104fb4a3877d02916434f598f2839f37ca4c
urbain32 <urbainq32@gmail.com>
2023-11-21 16:08:51 +0200

    anding the second controller

--- Module2/app.js
+++ Module2/app.js
@@ -1,6 +1,9 @@
 {
   (function () {
     "use strict";
-    angular.module("ShoppingListCheckOff", []).controller("ToBuyController ");
+    angular
+      .module("ShoppingListCheckOff", [])
+      .controller("ToBuyController ")
+      .controller("AlreadyBoughtController");
   })();
 }
